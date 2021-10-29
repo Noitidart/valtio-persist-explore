f5e1e805d50380a54c664176b69b25d6d9be3cc4
Noitidart <noitidart@gmail.com>
2021-10-28 21:44:22 -0700

    Got multi support but lost ability to debounce

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,6 @@
 import './App.css';
 
-import { debounce } from 'lodash';
+import { uniqueId } from 'lodash';
 import { useSnapshot } from 'valtio';
 
 import proxyWithPersist, {
@@ -11,20 +11,30 @@ const storage: ProxyPersistStorageEngine = {
   getItem: (name: string) => window.localStorage.getItem(name),
   setItem: (name: string, value: string) =>
     window.localStorage.setItem(name, value),
-  removeItem: (name: string) => window.localStorage.removeItem(name)
+  removeItem: (name: string) => window.localStorage.removeItem(name),
+  getAllKeys: () => {
+    return Object.keys(window.localStorage);
+  }
 };
 
+type PhotoId = string;
+const initialState: {
+  count: number;
+  photos: Record<PhotoId, { id: PhotoId; createdAt: number; clicks: number }>;
+} = {
+  count: 0,
+  photos: {}
+};
 const stateProxy = proxyWithPersist({
   name: 'app',
   getStorage: () => storage,
-  onBeforeWrite: debounce(write => write(), 1000, { maxWait: 1000 }),
+  // onBeforeWrite: debounce(write => write(), 1000, { maxWait: 1000 }),
   version: 0,
   persistPaths: {
-    count: 'single'
+    count: 'single',
+    photos: 'multi'
   },
-  initialState: {
-    count: 0
-  },
+  initialState,
   migrate: {}
 });
 
@@ -59,6 +69,49 @@ function App() {
         >
           Decrement
         </a>
+        <a
+          className="App-link"
+          href="#"
+          onClick={() => {
+            let photoId = uniqueId('photo');
+            while (photoId in stateProxy.photos) {
+              photoId = uniqueId('photo');
+            }
+            const photo = {
+              id: photoId,
+              createdAt: Date.now(),
+              clicks: 0
+            };
+
+            stateProxy.photos[photoId] = photo;
+          }}
+        >
+          Add Photo
+        </a>
+        {Object.entries(state.photos)
+          .sort((a, b) => a[1].createdAt - b[1].createdAt)
+          .map(([photoId, photo]) => (
+            <div key={photoId}>
+              <i>{photoId}</i>
+              <button
+                type="button"
+                onClick={() => {
+                  delete stateProxy.photos[photoId];
+                }}
+              >
+                delete
+              </button>
+              <button
+                type="button"
+                onClick={() => {
+                  stateProxy.photos[photoId].clicks++;
+                }}
+              >
+                Clicks: {photo.clicks}
+              </button>
+              {new Date(photo.createdAt).toLocaleTimeString()}
+            </div>
+          ))}
       </header>
     </div>
   );
--- src/proxyWithPersist.ts
+++ src/proxyWithPersist.ts
@@ -1,5 +1,5 @@
 import { get, set, toPath } from 'lodash';
-import { proxy } from 'valtio';
+import { proxy, snapshot } from 'valtio';
 import { subscribeKey } from 'valtio/utils';
 
 export type ProxyPersistStorageEngine = {
@@ -8,6 +8,7 @@ export type ProxyPersistStorageEngine = {
 
   setItem: (name: string, value: string) => void | Promise<void>;
   removeItem: (name: string) => void | Promise<void>;
+  getAllKeys: () => string[] | Promise<string[]>;
 };
 
 // "multi" only works for object type.
@@ -56,6 +57,7 @@ type IPersistedState<State extends object> = {
   );
 };
 
+let allKeysPromise: Promise<string[]>;
 export default function proxyWithPersist<S extends object>(
   inputs: IProxyWithPersistInputs<S>
 ) {
@@ -73,57 +75,171 @@ export default function proxyWithPersist<S extends object>(
     }
   });
 
-  Promise.all(
-    Object.entries(inputs.persistPaths).map(async function loadPath([
-      path,
-      technique
-    ]) {
-      const filePath = inputs.name + '-' + path;
+  (async function () {
+    if (!allKeysPromise) {
+      allKeysPromise = new Promise(async resolve => {
+        const allKeys = inputs.getStorage().getAllKeys();
+        resolve(allKeys);
+      });
+    }
+    const allKeys = await allKeysPromise;
+    console.log('allKeys:', allKeys);
 
-      const pathParts = toPath(path);
-      const pathStart = pathParts.slice(0, -1).join('');
-      const pathKey = pathParts.slice(-1)[0];
+    await Promise.all(
+      Object.entries(inputs.persistPaths).map(async function loadPath([
+        path,
+        technique
+      ]) {
+        const filePath = inputs.name + '-' + path;
 
-      const proxySubObject =
-        pathStart === '' ? proxyObject : get(proxyObject, pathStart);
+        const pathParts = toPath(path);
+        const pathStart = pathParts.slice(0, -1).join('');
+        const pathKey = pathParts.slice(-1)[0];
 
-      if (technique === 'single') {
-        const persistedString = await inputs.getStorage().getItem(filePath);
-        console.log('persistedString:', persistedString);
+        const proxySubObject =
+          pathStart === '' ? proxyObject : get(proxyObject, pathStart);
 
-        if (persistedString === null) {
-          // file does not exist
+        if (technique === 'single') {
+          const persistedString = await inputs.getStorage().getItem(filePath);
+          console.log('persistedString:', persistedString);
+
+          if (persistedString === null) {
+            // file does not exist
+          } else {
+            const persistedValue = JSON.parse(persistedString);
+            set(proxyObject, path, persistedValue);
+          }
+
+          subscribeKey(proxySubObject, pathKey, function persistPath(value) {
+            const write: Write = () => {
+              console.log(
+                'path changed persist it, path:',
+                path,
+                'value:',
+                value
+              );
+              inputs.getStorage().setItem(filePath, JSON.stringify(value));
+            };
+            onBeforeWrite(write, path);
+          });
+        } else if (technique === 'multi') {
+          await Promise.all(
+            allKeys.map(async leafPath => {
+              if (
+                leafPath.startsWith(inputs.name + '-') &&
+                toPath(leafPath).slice(0, -1).join('.') === filePath
+              ) {
+                const persistedString = await inputs
+                  .getStorage()
+                  .getItem(leafPath);
+                if (persistedString === null) {
+                  throw new Error(
+                    `Could not find file for leafPath found of "${leafPath}", this should not be possible as this was returned by inputs.getStorage().getAllKeys`
+                  );
+                }
+                console.log('persistedString:', persistedString);
+
+                const persistedValue = JSON.parse(persistedString);
+                set(
+                  proxyObject,
+                  leafPath.substring(inputs.name.length + '-'.length),
+                  persistedValue
+                );
+              }
+            })
+          );
+
+          let prevValue = snapshot(proxySubObject[pathKey]);
+          subscribeKey(
+            proxySubObject,
+            pathKey,
+            function persistLeaf(value, ...args) {
+              // figured out which subkeys were added, removed, changed
+              const keys = new Set(Object.keys(value));
+              const prevKeys = new Set(Object.keys(prevValue));
+
+              const possiblyUpdatedKeys = new Set(Object.keys(value));
+
+              const addedKeys: string[] = [];
+              keys.forEach(key => {
+                if (!prevKeys.has(key)) {
+                  addedKeys.push(key);
+                  possiblyUpdatedKeys.delete(key);
+                }
+              });
+
+              const removedKeys: string[] = [];
+              prevKeys.forEach(prevKey => {
+                if (!keys.has(prevKey)) {
+                  removedKeys.push(prevKey);
+                  possiblyUpdatedKeys.delete(prevKey);
+                }
+              });
+
+              console.log('possiblyUpdatedKeys:', possiblyUpdatedKeys);
+              const updatedKeys: string[] = [];
+              possiblyUpdatedKeys.forEach(possiblyUpdatedKey => {
+                const prevKeyValue = prevValue[possiblyUpdatedKey];
+                const keyValue = snapshot(value[possiblyUpdatedKey]);
+                if (prevKeyValue !== keyValue) {
+                  updatedKeys.push(possiblyUpdatedKey);
+                }
+              });
+
+              prevValue = snapshot(value);
+
+              if (
+                addedKeys.length ||
+                removedKeys.length ||
+                updatedKeys.length
+              ) {
+                const write: Write = async () => {
+                  console.log(
+                    JSON.stringify(
+                      {
+                        addedKeys,
+                        removedKeys,
+                        updatedKeys
+                      },
+                      null,
+                      2
+                    )
+                  );
+
+                  await Promise.all([
+                    ...removedKeys.map(key => {
+                      inputs.getStorage().removeItem(filePath + '.' + key);
+                    }),
+
+                    ...[...addedKeys, ...updatedKeys].map(key => {
+                      inputs
+                        .getStorage()
+                        .setItem(
+                          filePath + '.' + key,
+                          JSON.stringify(value[key])
+                        );
+                    })
+                  ]);
+                };
+
+                onBeforeWrite(write, path);
+              }
+            }
+          );
         } else {
-          const persistedValue = JSON.parse(persistedString);
-          set(proxyObject, path, persistedValue);
+          throw new Error(
+            `Unknown persist technique of "${technique}" for path "${filePath}".`
+          );
         }
+      })
+    );
 
-        subscribeKey(proxySubObject, pathKey, function persistPath(value) {
-          const write: Write = () => {
-            console.log(
-              'path changed persist it, path:',
-              path,
-              'value:',
-              value
-            );
-            inputs.getStorage().setItem(filePath, JSON.stringify(value));
-          };
-          onBeforeWrite(write, path);
-        });
-      } else if (technique === 'multi') {
-      } else {
-        throw new Error(
-          `Unknown persist technique of "${technique}" for path "${filePath}".`
-        );
-      }
-    })
-  ).then(() => {
     Object.assign(proxyObject._persist, {
       loaded: true,
       loading: false,
       status: 'loaded'
     });
-  });
+  })();
 
   return proxyObject;
 }
